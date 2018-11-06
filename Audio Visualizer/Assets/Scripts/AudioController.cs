6f57b14755d05f7ff377f7a2ce4e11f9ce2d59f6
Jonathan L <jlemanski1@gmail.com>
2018-11-05 17:08:52 -0700

    Basic Functionality
    -Basic visualization of 512 samples
    -Currently just gets samples every frame and compress to 512, then transform cubes based on samples

---
+++ Audio Visualizer/Assets/Scripts/AudioController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class AudioController : MonoBehaviour {
+    AudioSource _audioSource;
+    public static float[] samples = new float[512];   // Audio Samples: 512
+
+
+	void Start () {
+        _audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Get new samples every frame
+	void Update () {
+        GetSpectrumFromSource();
+	}
+
+    // Get samples from audio source and compress to 512 samples
+    void GetSpectrumFromSource() {
+        _audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ Audio Visualizer/Assets/Scripts/CubeVis.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeVis : MonoBehaviour {
+    public GameObject cube; // Cube Prefab
+    GameObject[] _cubes = new GameObject[512];
+	public float maxScale;
+    public int radius;
+
+	void Start () {
+        // Instantiate 512 cubes (1/sample) in a ring
+        for (int i = 0; i < 512; i++) {
+            GameObject _instanceCube = (GameObject)Instantiate(cube);
+            _instanceCube.transform.position = this.transform.position;
+            _instanceCube.transform.parent = this.transform;
+            _instanceCube.name = "Cube " + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0); // 512/360 = 0.703125
+            _instanceCube.transform.position = Vector3.forward * radius;
+            _cubes[i] = _instanceCube;
+        }
+	}
+	
+	// Scale Cubes with Sample to test visualization
+	void Update () {
+        for (int i = 0; i < 512; i++) {
+            if (cube != null) {
+                _cubes[i].transform.localScale = new Vector3(1, (AudioController.samples[i] * maxScale) + 2, 1);
+
+            }
+        }
+	}
+}
